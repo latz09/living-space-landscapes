e3ee36e2148e508887a1660b7794b9f0ae366c35
latz09 <85132328+latz09@users.noreply.github.com>
2022-08-12 11:20:31 -0500

    philosphy

--- components/homepage/Philosophy.js
+++ components/homepage/Philosophy.js
@@ -3,21 +3,18 @@ import SectionHeading from '../utils/SectionHeading';
 
 const Philosophy = () => {
 	return (
-		<div className='text-gray-500 grid prose font-long text-justify'>
-			<p className=' text-xl'>
+		<div className='text-gray-500 grid prose font-long text-center'>
+			<p className='text-lg  md:text-xl px-3'>
 				We create your landscape with a{' '}
 				<span className='text-landscape-700 '>naturalistic style </span>that
 				will inspire a sense of{' '}
 				<span className='text-landscape-700 '>calm </span>in those who
-				experience it. The landscape should provide highly{' '}
-				<span className='text-landscape-700 '> restorative garden views</span>{' '}
-				from inside the building and develop a desire to{' '}
-				<span className='text-landscape-700'> explore the outdoor </span>
-				space. The ideal landscape will connect the indoor and outdoor spaces
-				into a well used{' '}
-				<span className='text-landscape-700'> living space </span> that captures
-				your <span className='text-landscape-700'> imagination </span> and gives
-				you a sense of <span className='text-landscape-700'> serenity</span>.
+				experience it. The landscape should provide highly restorative garden
+				views from inside the building and develop a desire to explore the
+				outdoor space. The ideal landscape will <span className="text-landscape-700">connect</span> the indoor and outdoor
+				spaces into a well used living space that captures your imagination and
+				gives you a sense of{' '}
+				<span className='text-landscape-700'> serenity</span>.
 			</p>
 		</div>
 	);
