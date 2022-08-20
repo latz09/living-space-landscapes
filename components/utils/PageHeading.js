bd8f237337487ac45525b43b36ba2cbdf25c3b52
latz09 <85132328+latz09@users.noreply.github.com>
2022-08-20 11:54:16 -0500

    smaller padding for gallery on mobile

--- components/utils/PageHeading.js
+++ components/utils/PageHeading.js
@@ -1,6 +1,6 @@
 const PageHeading = ({ title }) => {
 	return (
-		<div className='sticky top-0 p-2 text-white font-thin tracking-widest shadow-md border text-2xl sm:text-6xl text-center z-10 bg-landscape-500 '>
+		<div className='sticky top-0 p-2 text-gray-100 tracking-widest shadow-md border text-2xl sm:text-6xl text-center z-10  bg-landscape-500 '>
 			{title}
 		</div>
 	);
