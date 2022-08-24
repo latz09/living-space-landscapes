d42a75c71e19719079b6361e0639f0fca6f73f1a
latz09 <85132328+latz09@users.noreply.github.com>
2022-08-24 15:42:31 -0500

    removed yellowish background from gallery immages

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -5,16 +5,13 @@ import { v4 as uuid } from 'uuid';
 
 const Gallery = () => {
 	return (
-		<div className='bg-gray-200 border-l-4 border-landscape-700 pt-16'>
-		
-			<div className='bg-landscape-300 max-w-7xl mx-auto px-2 md:px-4 border-x-8 border-landscape-500'>
-				<div className='grid xl:grid-cols-2 place-items-center  gap-4  bg-landscape-500 py-9'>
-					{galleryImages.map((image) => (
-						<div key={uuid()}>
-							<ImageItem image={image} />
-						</div>
-					))}
-				</div>
+		<div className='bg-landscape-500 max-w-7xl mx-auto px-2 md:px-4 border-x-8 border-landscape-700 '>
+			<div className='grid xl:grid-cols-2 place-items-center  gap-4  pt-24 pb-16'>
+				{galleryImages.map((image) => (
+					<div key={uuid()}>
+						<ImageItem image={image} />
+					</div>
+				))}
 			</div>
 		</div>
 	);
