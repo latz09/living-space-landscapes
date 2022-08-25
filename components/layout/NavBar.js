785a5b95a9ed815610639c6a873004818e27328d
latz09 <85132328+latz09@users.noreply.github.com>
2022-08-25 10:32:28 -0500

    final nav

--- components/layout/NavBar.js
+++ components/layout/NavBar.js
@@ -1,39 +1,30 @@
 import Link from 'next/link';
-import { RiMenu3Fill } from 'react-icons/ri';
+import { RiMenu3Fill, RiCloseLine } from 'react-icons/ri';
 import { useState } from 'react';
 import { MdHouseSiding } from 'react-icons/md';
-import { FaLeaf } from 'react-icons/fa';
 
 const Navbar = () => {
 	const navLinks = [
 		{
-			name: 'Japanese gardens',
+			name: 'Japanese Gardens',
 			link: '/japanese-gardens',
-			featured: true,
 		},
-
 		{
 			name: 'Why Us',
 			link: '/why-us',
-			featured: false,
 		},
 		{
 			name: 'Services',
 			link: '/services',
-			featured: false,
 		},
-
-		//FOR NAVBAR IF FEATURED EQUALS TRUE PUT THE LEAVE...HAVE THE LEAF GREEN. HOPEFULLY CAN GIVE A LITTLE SPACE
 
 		{
 			name: 'Contact',
 			link: '/contact',
-			featured: false,
 		},
 		{
 			name: 'Gallery',
 			link: '/gallery',
-			featured: false,
 		},
 	];
 
@@ -45,10 +36,10 @@ const Navbar = () => {
 	return (
 		<div className='shadow-md w-full fixed top-0 left-0 z-10'>
 			<div className='bg-landscape-500'>
-				<div className='max-w-7xl mx-auto flex items-center justify-between py-4 md:px-10 px-7 text-gray-50 tracking-wider'>
+				<div className='max-w-7xl mx-auto flex items-center justify-between py-4 md:px-10 px-7 text-gray-50 tracking-widest'>
 					<Link href={'/'}>
 						<span
-							className='text-landscape-700  transition duration-700 text-4xl md:text-5xl cursor-pointer'
+							className='hover:text-landscape-700 duration-700 text-4xl cursor-pointer'
 							onClick={closeMenu}
 						>
 							<MdHouseSiding />{' '}
@@ -59,11 +50,11 @@ const Navbar = () => {
 						onClick={() => setOpen(!open)}
 						className='text-3xl  cursor-pointer md:hidden'
 					>
-						<RiMenu3Fill />
+						{!open ?  <RiMenu3Fill /> : <RiCloseLine />}
 					</button>
 
 					<ul
-						className={` bg-landscape-500 md:flex md:items-center md:pb-0 pb-12 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto transition-all duration-700 ease-in ${
+						className={` bg-landscape-500 border-b-4 border-landscape-700 md:border-none md:flex md:items-center md:pb-0 pb-12 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto transition-all duration-700 ease-in ${
 							open ? 'top-16' : 'top-[-490px]'
 						}`}
 					>
@@ -74,7 +65,7 @@ const Navbar = () => {
 								onClick={() => setOpen(!open)}
 							>
 								<Link href={link.link}>
-									<a className={`hover:text-landscape-700 duration-700 border-b pb-2 md:border-none md:pb-0 ${link.featured === true && 'underline underline-offset-8 decoration-2 decoration-landscape-700 hover:decoration-gray-50 transition duration-700 border-none'}`}>
+									<a className='hover:text-landscape-700 duration-700 border-b pb-2 md:border-none md:pb-0'>
 										{link.name}
 									</a>
 								</Link>
