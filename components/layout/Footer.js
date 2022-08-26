b2f240ae53cb98af1ee451a7940fce3fc3d1eb2b
latz09 <85132328+latz09@users.noreply.github.com>
2022-08-26 10:30:39 -0500

    d

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
 		<div className='bg-landscape-500 w-full bottom-0 '>
 			<Link href={'/contact'}>
 				<div className='py-8 text-white text-xl md:text-3xl flex space-x-3 justify-center items-center cursor-pointer hover:opacity-75 transition duration-700'>
-					<span> Set up Consultation </span>{' '}
+					<span>git Set up Consultation</span>{' '}
 					<span>
 						<BsArrowBarRight />{' '}
 					</span>
