3195ebbcfd4684e8e1e45ea435a3058b01feb588
latz09 <85132328+latz09@users.noreply.github.com>
2022-08-26 10:37:51 -0500

    added height to logo

--- components/layout/heros/Hero.js
+++ components/layout/heros/Hero.js
@@ -1,12 +1,11 @@
 import Image from 'next/image';
 import heroImage from '../../../public/images/gallery-images/11.jpg';
-import logo from '../../../public/images/logos/logo.png'
-
+import logo from '../../../public/images/logos/logo.png';
 
 const Hero = () => {
 	return (
 		<div className='relative w-full h-[75vh]'>
-			<div className='z-10  w-full h-'>
+			<div className='z-10'>
 				<Image
 					src={heroImage}
 					alt=''
@@ -20,14 +19,14 @@ const Hero = () => {
 				/>
 			</div>
 			<div className='relative flex justify-center items-center h-full bg-black bg-opacity-60'>
-				<div className='lg:w-1/2 max-w-3xl px-4'>
-					<Image src ={logo} alt='' layout='intrinsic' />				
+				<div className='lg:w-1/2 max-w-3xl'>
+					<div className="h-full w-full">
+						<Image src={logo} alt='' layout='intrinsic' />
+					</div>
 				</div>
-
 			</div>
 		</div>
 	);
 };
 
 export default Hero;
-
