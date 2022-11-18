78c5d811585e5b88212056bf84b059aee9d5336c
latz09 <85132328+latz09@users.noreply.github.com>
2022-11-18 13:44:44 -0600

    whoops, fixed navbar

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -3,9 +3,8 @@ import Navbar from './NavBar';
 const Layout = ({ children }) => {
 	return (
 		<div className='flex flex-col h-screen'>
-			<div className="shadow-2xl">
-				<Navbar />
-			</div>
+			<Navbar />
+
 			<main className='flex-grow bg-[#F2F8EF]'>{children}</main>
 			<Footer />
 		</div>
