56ff6220a6804f8cbc582ebc2d90b70da012f58c
latz09 <85132328+latz09@users.noreply.github.com>
2022-12-01 12:46:47 -0600

    home page tweeks

--- components/layout/heros/SectionHero.js
+++ components/layout/heros/SectionHero.js
@@ -4,7 +4,8 @@ import { motion } from 'framer-motion';
 
 const SectionHero = ({ bgImage, title, linkTo, buttonText, description }) => {
 	return (
-		<div className='relative w-full h-[70vh]  tracking-widest text-white/80'>
+		
+		<div className='relative w-full h-[40vh] lg:h-[70vh]  tracking-widest text-white/80'>
 			<Image
 				src={bgImage}
 				alt=''
@@ -15,18 +16,19 @@ const SectionHero = ({ bgImage, title, linkTo, buttonText, description }) => {
 			/>
 
 			<div className='relative h-full bg-black/60 grid place-items-center'>
-				<motion.div className='max-w-7xl grid gap-4 md:gap-8'>
-					<div className='grid gap-1 text-center'>
+				<motion.div className='max-w-7xl grid gap-4 md:gap-6'>
+					<div className='grid gap-2 text-center'>
 						<motion.h1
 							initial={{ opacity: 0, scale: 0.96 }}
 							whileInView={{ opacity: 1, scale: 1 }}
 							transition={{ delay: 0.2, duration: 1.2 }}
-							className='text-4xl sm:text-5xl md:text-7xl text-landscape-500 '
+							className='tracking-wider text-5xl md:text-6xl lg:text-7xl text-landscape-500 '
 						>
 							{title}
 						</motion.h1>
+
 						<motion.div
-							className='text-base sm:text-lg md:text-xl lg:text-2xl px-2 mx-auto font-bold'
+							className='text-base sm:text-lg lg:text-2xl px-2 mx-auto font-bold md:font-normal tracking-widest italic'
 							initial={{ opacity: 0 }}
 							whileInView={{ opacity: 1 }}
 							transition={{ delay: 0.8, duration: 1.4 }}
@@ -41,7 +43,7 @@ const SectionHero = ({ bgImage, title, linkTo, buttonText, description }) => {
 							delay: 1.3,
 							duration: 1.5,
 						}}
-						className=' w-3/4 md:w-2/3 mx-auto shadow-lg'
+						className=' w-1/2 md:w-3/5 mx-auto shadow-lg'
 					>
 						<ButtonLink href={linkTo} title={buttonText} />
 					</motion.div>
