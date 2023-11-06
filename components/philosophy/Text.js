6a371ce6951f0c0a703b7f94da4142d4ddcae801
latz09 <85132328+latz09@users.noreply.github.com>
2023-11-06 15:05:17 -0600

    natrualistic

--- components/philosophy/Text.js
+++ components/philosophy/Text.js
@@ -2,7 +2,7 @@ const Text = () => {
 	return (
 		<div className='text-gray-600 grid font-logo prose tracking-wide '>
 			<p className='text-lg  md:text-xl mx-4 lg:mx-0 '>
-				We create your landscape with a naturitic style that will inspire a
+				We create your landscape with a naturalistic style that will inspire a
 				sense of calm in those who experience it. The landscape should provide
 				highly restorative garden views from inside the building and develop a
 				desire to explore the outdoor space. The ideal landscape will connect
