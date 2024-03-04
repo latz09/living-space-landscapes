c8c3fb1770c768426fe39a2d3c5be0aaead9548a
latz09 <85132328+latz09@users.noreply.github.com>
2024-03-04 14:36:27 -0600

    tweaked gallery

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -13,22 +13,21 @@ const Gallery = () => {
 				}
 			/>
 
-			<div className='grid lg:grid-cols-2 xl:grid-cols-3 gap-8 place-items-center px-1 md:px-[5vw] py-10 lg:py-20 bg-landscape-500/30'>
+			<div className=' grid lg:grid-cols-2 xl:grid-cols-3 gap-16 place-items-center  md:px-[5vw] py-10 lg:py-20 bg-landscape-500/30'>
 				{galleryImages.map((image, index) => (
-					<motion.div
+					<div
 						key={index}
-						whileHover={{ scale: 1.2, zIndex: 10 }}
-						transition={{ duration: 0.4 }}
+						
 					>
 						<motion.div
-							className='hover:shadow-xl grid place-items-center transition duration-700'
-							initial={{ opacity: 0 }}
-							whileInView={{ opacity: 1 }}
-							transition={{ delay: 0.3, duration: 1.1 }}
+							className='shadow-lg shadow-dark/50 grid place-items-center transition duration-700'
+							initial={{  y: 100 }}
+							whileInView={{ opacity: 1, y: 0}}
+							transition={{  duration: .4 }}
 						>
-							<Image src={image.image} alt={image.alt} />
+							<Image src={image.image} alt={image.alt} priority={true} />
 						</motion.div>
-					</motion.div>
+					</div>
 				))}
 			</div>
 		</>
